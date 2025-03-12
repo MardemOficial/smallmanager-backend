dee145fc637ce6ae444488119e95680c882c6730
Mardem Paz <mardenexecutive@gmail.com>
2025-03-11 22:22:02 -0300

    feat: Changing the table name.

--- src/main/java/br/com/SmallManager/domain/SystemUser.java
+++ src/main/java/br/com/SmallManager/domain/SystemUser.java
@@ -3,14 +3,18 @@ package br.com.SmallManager.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "SystemUser")
-public class SystemUser {
+@Table(name = "users")
+public class SystemUser implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -18,4 +22,33 @@ public class SystemUser {
     private String name;
     private String password;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
