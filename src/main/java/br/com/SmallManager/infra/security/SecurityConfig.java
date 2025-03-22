f544cbbbcf7ddaad47f1687b27860fecc2448f04
Mardem Paz <mardenexecutive@gmail.com>
2025-03-21 21:45:41 -0300

    feat: Add CORS configuration to the application.

--- src/main/java/br/com/SmallManager/infra/security/SecurityConfig.java
+++ src/main/java/br/com/SmallManager/infra/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package br.com.SmallManager.infra.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,28 +13,31 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final TokenAcessFilter tokenAcessFilter;
 
-    public SecurityConfig(TokenAcessFilter tokenAcessFilter) {
-        this.tokenAcessFilter = tokenAcessFilter;
-    }
-
     @Bean
     public SecurityFilterChain filterSecurity(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(
                         req -> {
-                            req.requestMatchers("/login", "/swagger-ui/**", "/v3/api-docs/**").permitAll();
+                            req.requestMatchers("/status","/login", "/swagger-ui/**", "/v3/api-docs/**").permitAll();
                             req.anyRequest().authenticated();
                         }
                 )
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .addFilterBefore(tokenAcessFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
@@ -45,6 +50,17 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedOrigins(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
     }
 
 }
